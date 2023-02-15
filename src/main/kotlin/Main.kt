42a9397349b0c190a95779b68a4d0683543fdd02
Loice-KaniniMwau <mwauloice@gmail.com>
2023-02-15 14:53:39 +0300

    kotlin strings assignment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,43 +2,48 @@ fun main() {
     school("akirachix")
     println(identity(27,"margaret"))
     println(thesize("akirachix"))
+    println(myname("joanne"))
+    println()
 
-    //that's me
-    val identity1="maryanne"
-    val identity2="maryanne"
-    if (identity1==identity2){
+    }
+
+
+
+            //question1
+           fun school(institution: String) {
+            println(institution[0])
+            println(institution[2])
+            println(institution[3])
+
+        }
+           //question2
+        fun identity(age: Int, name: String): String {
+            return ("Hi, My name is $name and I am $age years old")
+
+        }
+              //question3
+        fun thesize(thesize: String): Int {
+            return (thesize.length)
+
+
+        }
+
+      //I don't know who that is
+      fun myname(myname:String) {
+       val nickname = "maryanne"
+       if (myname == nickname) {
         println("that's me")
-    }else {
+        } else {
         println("I don't know who that is")
+        }
+
+
+
+
+
+
+
+
+
+
     }
-    // I don't know who that is
-    val name1="maryanne"
-    val name2="faith"
-    if(name1==name2){
-        println("Tha's me")
-    }else{
-        println("I don't know who that is")
-    }
-
-
-
-
-}
-    fun school(institution:String){
-     println(institution[0])
-    println(institution[2])
-    println(institution[3])
-
-}
-
-fun identity(age: Int,name:String):String{
-   return("My name is $name and I am $age years old")
-
-}
-fun thesize(thesize:String):Int{
-    return(thesize.length)
-
-
-}
-
-
